145001b641c683f37b7e79bbb2236d8c2c10e4f1
Natalie Wright <coereba.lover@gmail.com>
2015-02-10 14:23:48 -0500

    removed genera with <10 samples from dataset for analysis

--- island-rule-test.R
+++ island-rule-test.R
@@ -14,5 +14,24 @@ sub.data <- subset(data, keel.length != 'NA' & coracoid != 'NA' & femur != 'NA' 
 summary(sub.data)
 sub.data <- droplevels(sub.data)
 
-summary(sub.data$genus)
+# PCA across all taxa to correct for body size
+pca.data <- sub.data[, c('coracoid', 'femur', 'humerus', 'tarsometatarsus')]
+pca <- prcomp(pca.data, scale = T)
+summary(pca)
+pca
+sub.data$pc1 <- pca$x[, 1]*-1
+
+# automate extracting coefficients from linear models for each genus with >=10 samples & >3 island populations
+# first, remove genera with <10 samples
+# get count of how many samples per genus
+genus.sample <- as.data.frame(table(sub.data$genus))
+# create vector of genera with <10 samples
+few.samples <- subset(genus.sample, Freq < 10)$Var1
+few.samples
+# remove genera with <10 samples from dataset
+dataset <- subset(sub.data, !(genus %in% few.samples))
+summary(dataset)
+summary(dataset$genus)
+dataset <- droplevels(dataset)
+
 
